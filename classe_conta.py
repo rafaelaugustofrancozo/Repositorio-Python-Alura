ac918e19fbf95c5d1988fd006f11e989f4f01d0f
rafaelaugustofrancozo <faelaugusto88@hotmail.com>
2022-12-23 00:18:32 -0300

    Classes, métodos, encapsulamento, getters e setters

--- classe_conta.py
+++ classe_conta.py
@@ -1,22 +1,44 @@
 class conta_banco:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'Saldo da conta do titular {self.titular}: R${self.saldo}')
-
-    def deposita(self, valor):
-        self.saldo += valor
-        print(f'Depósito de R${valor} efetuado! Novo saldo: R${self.saldo}')
+        print(f'Saldo da conta do titular {self.__titular}: R${self.__saldo}')
 
     def saca(self, valor):
-        self.saldo -= valor
-        print(f'Saque de R${valor} efetuado! Novo saldo: R${self.saldo}')
+        self.__saldo -= valor
+        print(f'Saque de R${valor} efetuado! Novo saldo: R${self.__saldo}')
+
+    def deposita(self, valor):
+        self.__saldo += valor
+        print(f'Depósito de R${valor} efetuado! Novo saldo: R${self.__saldo}')
 
     def amplia_limite(self, valor):
-        self.limite += valor
-        print(f'Limite ampliado para R${self.limite}!')
+        self.__limite += valor
+        print(f'Limite ampliado para R${self.__limite}!')
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_saldo(self, saldo):
+        self.__saldo += saldo
+
+    def set_titular(self, titular):
+        self.__titular = titular
+
+    def set_limite(self, limite):
+        self.__limite += limite
